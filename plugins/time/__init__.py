f5efad6e24eabcd533df69856c5526b0219a2b24
Cedric Boverie <ced@boverie.eu>
2012-04-10 22:11:17 +0200

    Update time plugin

--- plugins/time/__init__.py
+++ plugins/time/__init__.py
@@ -1,142 +1,150 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import re
-import urllib2, urllib
-import json
-
 from plugin import *
-
-from siriObjects.uiObjects import AddViews, AssistantUtteranceView
 from siriObjects.clockObjects import ClockSnippet, ClockObject
+import json
+import random
+import types
+import urllib
+import urllib2
 
-####### geonames.org API username ######
-geonames_user="test2"
 
-class timePlugin(Plugin):
-    
-    localizations = {"currentTime": 
-                        {"search":{"de-DE": "Es wird gesucht ...", "en-US": "Looking up ...", "fr-FR": "Je cherche..."}, 
-                         "currentTime": {"de-DE": "Es ist @{fn#currentTime}", "en-US": "It is @{fn#currentTime}", "fr-FR": u"Il est @{fn#currentTime}"}}, 
-                     "currentTimeIn": 
-                        {"search":{"de-DE": "Es wird gesucht ...", "en-US": "Looking up ...", "fr-FR": "Je cherche..."}, 
-                         "currentTimeIn": 
-                                {
-                                "tts": {"de-DE": u"Die Uhrzeit in {0},{1} ist @{{fn#currentTimeIn#{2}}}:", "en-US": "The time in {0},{1} is @{{fn#currentTimeIn#{2}}}:", "fr-FR": u"A {0},{1}, il est @{{fn#currentTimeIn#{2}}}:"},
-                                "text": {"de-DE": u"Die Uhrzeit in {0}, {1} ist @{{fn#currentTimeIn#{2}}}:", "en-US": "The time in {0}, {1} is @{{fn#currentTimeIn#{2}}}:", "fr-FR":u"A {0}, {1}, il est @{{fn#currentTimeIn#{2}}}:"}
-                                }
-                        },
-                    "failure": {
-                                "de-DE": "Ich kann dir die Uhr gerade nicht anzeigen!", "en-US": "I cannot show you the clock right now", "fr-FR": u"Désolé, j'ai perdu ma montre."
-                                }
-                    }
 
-    @register("de-DE", "(Wie ?viel Uhr.*)|(.*Uhrzeit.*)")     
+
+localizations = {
+    "search":
+    {
+      "de-DE": [u"Es wird gesucht ..."],
+      "en-US": [u"Looking up ..."],
+      "fr-FR": [u"Je cherche..."],
+    },
+    "currentTime":
+    {
+     "de-DE": [u"Es ist @{fn#currentTime}"],
+     "en-US": [u"It is @{fn#currentTime}"],
+     "fr-FR": [u"Il est @{fn#currentTime}"]
+    },
+    "currentTimeIn":
+    {
+      "de-DE": [u"Die Uhrzeit in {0} ist @{{fn#currentTimeIn#{1}}}:"],
+      "en-US": [u"The time in {0} is @{{fn#currentTimeIn#{1}}}:"],
+      "fr-FR": [u"A {0}, il est @{{fn#currentTimeIn#{1}}} :"],
+    },
+    "failure":
+    {
+      "de-DE": [u"Es tut mir leid aber für eine Anfrage habe ich keine Uhrzeit."],
+      "en-US": [u"I'm sorry but I don't have a time for this request"],
+      "fr-FR": [u"Désolé, j'ai perdu ma montre."],
+    }
+}
+def getNameFromGoogle(request):
+    try:
+        result = getWebsite(request, timeout=5)
+        root = json.loads(result)
+        location = root["results"][0]["formatted_address"]
+        return location
+    except:
+        return None
+
+
+
+class currentTime(Plugin):
+
+    def showWait(self, language):
+        textView = UIAssistantUtteranceView()
+        textView.speakableText = textView.text = random.choice(localizations['search'][language])
+        textView.dialogIdentifier = "Clock#getTime"
+
+        rootAnchor = UIAddViews(self.refId)
+        rootAnchor.dialogPhase = rootAnchor.DialogPhaseReflectionValue
+        rootAnchor.scrollToTop = False
+        rootAnchor.temporary = False
+        rootAnchor.views = [textView]
+
+        self.sendRequestWithoutAnswer(rootAnchor)
+
+    @register("de-DE", "(Wie ?viel Uhr.*)|(.*Uhrzeit.*)")
     @register("en-US", "(What.*time.*)|(.*current time.*)")
     @register("fr-FR", "(.*Quel.*heure.*)|(.*heure actuelle.*)")
     def currentTime(self, speech, language):
         #first tell that we look it up
-        view = AddViews(self.refId, dialogPhase="Reflection")
-        view.views = [AssistantUtteranceView(text=timePlugin.localizations['currentTime']['search'][language], speakableText=timePlugin.localizations['currentTime']['search'][language], dialogIdentifier="Clock#getTime")]
-        self.sendRequestWithoutAnswer(view)
-        
-        # tell him to show the current time
-        view = AddViews(self.refId, dialogPhase="Summary")
-        view1 = AssistantUtteranceView(text=timePlugin.localizations['currentTime']['currentTime'][language], speakableText=timePlugin.localizations['currentTime']['currentTime'][language], dialogIdentifier="Clock#showTimeInCurrentLocation")
+        self.showWait(language)
+
+
+        textView = UIAssistantUtteranceView()
+        textView.text = textView.speakableText = random.choice(localizations["currentTime"][language])
+        textView.dialogIdentifier = "Clock#showTimeInCurrentLocation"
+        textView.listenAfterSpeaking = False
+
         clock = ClockObject()
         clock.timezoneId = self.connection.assistant.timeZoneId
-        view2 = ClockSnippet(clocks=[clock])
-        view.views = [view1, view2]
-        self.sendRequestWithoutAnswer(view)
-        self.complete_request()
-    
-    @register("de-DE", "(Wieviel Uhr.*in ([\w ]+))|(Uhrzeit.*in ([\w ]+))")
-    @register("en-US", "(What.*time.*in ([\w ]+))|(.*current time.*in ([\w ]+))")
-    @register("fr-FR", u"(Quel.*heure.*(à|a|en) ([\w ]+))|(.*heure actuelle.*(à|a|en) ([\w ]+))")
-    def currentTimeIn(self, speech, language):
-        view = AddViews(self.refId, dialogPhase="Reflection")
-        view.views = [AssistantUtteranceView(text=timePlugin.localizations['currentTimeIn']['search'][language], speakableText=timePlugin.localizations['currentTimeIn']['search'][language], dialogIdentifier="Clock#getTime")]
-        self.sendRequestWithoutAnswer(view)
-        
-        error = False
-        countryOrCity = re.match(u"(?u).* (in|a|à|en) ([\w ]+)$", speech, re.IGNORECASE)
-        if countryOrCity != None:
-            countryOrCity = countryOrCity.group(countryOrCity.lastindex).strip()
-            # lets see what we got, a country or a city... 
-            # lets use google geocoding API for that
-            url = u"http://maps.googleapis.com/maps/api/geocode/json?address={0}&sensor=false&language={1}".format(urllib.quote_plus(countryOrCity.encode("utf-8")), language)
-            # lets wait max 3 seconds
-            jsonString = None
-            try:
-                jsonString = urllib2.urlopen(url, timeout=3).read()
-            except:
-                pass
-            if jsonString != None:
-                response = json.loads(jsonString)
-                # lets see what we have...
-                if response['status'] == 'OK':
-                    components = response['results'][0]['address_components']
-                    types = components[0]['types'] # <- this should be the city or country
-                    if "country" in types:
-                        # OK we have a country as input, that sucks, we need the capital, lets try again and ask for capital also
-                        components = filter(lambda x: True if "country" in x['types'] else False, components)
-                        url = u"http://maps.googleapis.com/maps/api/geocode/json?address=capital%20{0}&sensor=false&language={1}".format(urllib.quote_plus(components[0]['long_name'].encode("utf-8")), language)
-                            # lets wait max 3 seconds
-                        jsonString = None
-                        try:
-                            jsonString = urllib2.urlopen(url, timeout=3).read()
-                        except:
-                            pass
-                        if jsonString != None:
-                            response = json.loads(jsonString)
-                            if response['status'] == 'OK':
-                                components = response['results'][0]['address_components']
-                # response could have changed, lets check again, but it should be a city by now 
-                if response['status'] == 'OK':
-                    # get latitude and longitude
-                    location = response['results'][0]['geometry']['location']
-                    url = u"http://api.geonames.org/timezoneJSON?lat={0}&lng={1}&username={2}".format(location['lat'], location['lng'], geonames_user)
-                    jsonString = None
-                    try:
-                        jsonString = urllib2.urlopen(url, timeout=3).read()
-                    except:
-                        pass
-                    if jsonString != None:
-                        timeZoneResponse = json.loads(jsonString)
-                        if "timezoneId" in timeZoneResponse:
-                            timeZone = timeZoneResponse['timezoneId']
-                            city = filter(lambda x: True if "locality" in x['types'] or "administrative_area_level_1" in x['types'] else False, components)[0]['long_name']
-                            country = filter(lambda x: True if "country" in x['types'] else False, components)[0]['long_name']
-                            countryCode = filter(lambda x: True if "country" in x['types'] else False, components)[0]['short_name']
-                            
-                            view = AddViews(self.refId, dialogPhase="Summary")
-                            view1 = AssistantUtteranceView(text=timePlugin.localizations['currentTimeIn']['currentTimeIn']['text'][language].format(city, country, timeZone), speakableText=timePlugin.localizations['currentTimeIn']['currentTimeIn']['tts'][language].format(city, country, timeZone), dialogIdentifier="Clock#showTimeInOtherLocation")
-                            clock = ClockObject()
-                            clock.timezoneId = timeZone
-                            clock.countryCode = countryCode
-                            clock.countryName = country
-                            clock.cityName = city
-                            clock.unlocalizedCityName = city
-                            clock.unlocalizedCountryName = country
-                            view2 = ClockSnippet(clocks=[clock])
-                            view.views = [view1, view2]
-                            self.sendRequestWithoutAnswer(view)
-                        else:
-                            error = True
-                    else:
-                        error = True
-                else:
-                    error = True
-            else:
-                error = True
-        else:
-            error = True
-        if error:
-            view = AddViews(self.refId, dialogPhase="Completion")
-            view.views = [AssistantUtteranceView(text=timePlugin.localizations['failure'][language], speakableText=timePlugin.localizations['failure'][language], dialogIdentifier="Clock#cannotShowClocks")]
-            self.sendRequestWithoutAnswer(view)
+
+        clockView = ClockSnippet()
+        clockView.clocks = [clock]
+
+        rootAnchor = UIAddViews(self.refId)
+        rootAnchor.dialogPhase = rootAnchor.DialogPhaseSummaryValue
+        rootAnchor.views = [textView, clockView]
+
+
+        self.sendRequestWithoutAnswer(rootAnchor)
         self.complete_request()
 
+    @register("de-DE", "(Wieviel Uhr.*in|Uhrzeit.*in) (?P<loc>[\w ]+)")
+    @register("en-US", "(What.*time.*in|.*current time.*in) (?P<loc>[\w ]+)")
+    @register("fr-FR", u"(Quel.*heure|.*heure actuel).*(à|a|en) (?P<loc>[\w ]+)")
+    def currentTimeIn(self, speech, language, matchedRegex):
+
+        self.showWait(language)
+
+        location = matchedRegex.group("loc")
+        # ask google to enhance the request
+        googleGuesser = "http://maps.googleapis.com/maps/api/geocode/json?address={0}&sensor=false&language={1}".format(urllib.quote(location.encode("utf-8")), language)
+        googleLocation = getNameFromGoogle(googleGuesser)
+        if googleLocation != None:
+            location = googleLocation
+
+        self.logger.debug(u"User requested time in: {0}".format(location))
+        # ask yahoo for a timezoneID
+        query = u"select name from geo.places.belongtos where member_woeid in (select woeid from geo.places where text=\"{0}\") and placetype=31".format(location.encode("utf-8"))
+        request = u"http://query.yahooapis.com/v1/public/yql?q={0}&format=json&callback=".format(urllib.quote(query.encode("utf-8")))
+        timeZoneId = None
+        try:
+            result = getWebsite(request, timeout=5)
+            root = json.loads(result)
+            place = root["query"]["results"]["place"]
+            if type(place) == types.ListType:
+                place = place[0]
+
+            timeZoneId = place["name"]
+        except:
+            self.logger.exception("Error getting timezone")
+
+        if timeZoneId == None:
+            self.say(random.choice(localizations['failure'][language]))
+            self.complete_request()
+            return
+
+        clock = ClockObject()
+        clock.timezoneId = timeZoneId
+
+        clockView = ClockSnippet()
+        clockView.clocks = [clock]
+
+        textView = UIAssistantUtteranceView()
+        textView.listenAfterSpeaking = False
+        textView.dialogIdentifier = "Clock#showTimeInOtherLocation"
+        textView.text = textView.speakableText = random.choice(localizations["currentTimeIn"][language]).format(location, timeZoneId)
+
+        rootAnchor = UIAddViews(self.refId)
+        rootAnchor.dialogPhase = rootAnchor.DialogPhaseSummaryValue
+        rootAnchor.scrollToTop = False
+        rootAnchor.temporary = False
+        rootAnchor.views = [textView, clockView]
+
+        self.sendRequestWithoutAnswer(rootAnchor)
+        self.complete_request()
 
 ## we should implement such a command if we cannot get the location however some structures are not implemented yet
 #{"class"=>"AddViews",
